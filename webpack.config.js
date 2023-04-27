2eecfac5ac9b21e2c5b07ecfc0e96749266e9a85
Ken Bimmel <you@example.com>
2023-04-26 22:56:59 -0500

    Try switching to legacy version of MUI

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const config = {
+  resolve: {
+    alias: {
+      "@mui/base": "@mui/base/legacy",
+      "@mui/lab": "@mui/lab/legacy",
+      "@mui/material": "@mui/material/legacy",
+      "@mui/styled-engine": "@mui/styled-engine/legacy",
+      "@mui/system": "@mui/system/legacy",
+      "@mui/utils": "@mui/utils/legacy",
+    },
+  },
+};
+
+export default config;
